f57cd0bf0487a4c61b249b44ac6bd42ef0b6e5de
Ross Landino <mrlandino@gmail.com>
2023-01-12 11:24:26 -0700

    Adding image to directory

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,31 @@
-
+import { Grid, Box, Container } from "@mui/material";
 
 const Home = () => {
+
+  const styles = {
+    imageContainer: {
+      backgroundImage: `https://www.freepnglogos.com/uploads/mars-png/mars-transparent-png-stickpng-0.png`
+    }
+  }
  return (
   <>
-    <h1>Home</h1>
+    <Grid>
+      <Container maxWidth='sm'>
+        <Box 
+          component='img'
+          sx={{
+            height:700,
+            width: 650,
+            maxHeight: {xs: 350, md: 720},
+            maxWidth: {xs: 350, md: 725}
+          }}
+          alt='Planet Mars'
+          src='https://e7.pngegg.com/pngimages/629/223/png-clipart-mars-mars-thumbnail.png
+          '
+        />
+      </Container>
+    </Grid>
+
   </>
  )
 }
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -58,7 +58,7 @@ const Nav = () => {
         </Container>
       </AppBar>
     </>
-  );
+  )
 }
 
 export default Nav;
